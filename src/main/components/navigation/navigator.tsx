4763704dd25a088418c63d2dd7d605e6f57b62b5
incendiarybean <ben.weare12@gmail.com>
2022-11-29 15:06:17 +0000

    Updated release tag

--- src/main/components/navigation/navigator.tsx
+++ src/main/components/navigation/navigator.tsx
@@ -20,7 +20,7 @@ export const Navigator = ({
                             <span className="">Isaac Achievement Hunter</span>
                         </a>
                         <a
-                            href="#latestrelease-TODO"
+                            href="https://github.com/incendiarybean/isaac-achievement-hunter/releases/tag/v0.2.0-beta"
                             className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden sm:flex items-center hover:bg-sky-400/20"
                         >
                             <strong className="font-semibold">
