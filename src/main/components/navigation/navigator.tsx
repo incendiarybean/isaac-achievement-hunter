b7623671013005f53c57929dd2237606954989d4
incendiarybean <ben.weare12@gmail.com>
2022-12-14 16:20:45 +0000

    Bumped to 0.2.1

--- src/main/components/navigation/navigator.tsx
+++ src/main/components/navigation/navigator.tsx
@@ -20,11 +20,11 @@ export const Navigator = ({
                             <span className="">Isaac Achievement Hunter</span>
                         </a>
                         <a
-                            href="https://github.com/incendiarybean/isaac-achievement-hunter/releases/tag/v0.2.0-beta"
+                            href="https://github.com/incendiarybean/isaac-achievement-hunter/releases/tag/v0.2.1"
                             className="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 hidden sm:flex items-center hover:bg-sky-400/20"
                         >
                             <strong className="font-semibold">
-                                IAH V0.2.0
+                                IAH V0.2.1
                             </strong>
                         </a>
                         <DesktopNav
